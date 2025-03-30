b4add3fdd47b0d3eac5971dbc3d8ce2fe88eb100
Alexandre Fernandez <alexandre.fernandez@enti.cat>
2025-03-30 13:34:35 +0200

    Implementamos movimiento y disparo

--- SpaceInvadersUnity/Assets/Scripts/Player/PlayerController.cs
+++ SpaceInvadersUnity/Assets/Scripts/Player/PlayerController.cs
@@ -4,14 +4,23 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
     public float boundary = 8f;
+    public GameObject bulletPrefab;
+    public Transform firePoint;
 
     void Update()
     {
+
         float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         Vector3 newPos = transform.position + Vector3.right * move;
 
-  
+      
         newPos.x = Mathf.Clamp(newPos.x, -boundary, boundary);
         transform.position = newPos;
+
+   
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        }
     }
 }
